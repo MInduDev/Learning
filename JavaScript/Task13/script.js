e3c9c88fe037aefa885fcf081dbec3171616b695
M Indu <maddhi.indu@gmail.com>
2026-06-29 23:18:23 +0530

    Create script.js

---
+++ JavaScript/Task13/script.js
@@ -0,0 +1,28 @@
+let number1 = 5;
+let number1 = 10;
+let number1 = 15;
+let number1 = 20;
+
+if (number1 % 2 === 0){
+    console.log("The number" + number1 + "is even");
+} else {
+    console.log("The number" + number1 + "is odd");
+}
+
+if (number2 % 2 === 0){
+    console.log("The number" + number2 + "is even");
+} else {
+    console.log("The number" + number2 + "is odd");
+}
+
+if (number3 % 2 === 0){
+    console.log("The number" + number3 + "is even");
+} else {
+    console.log("The number" + number3 + "is odd");
+}
+
+if (number4 % 2 === 0){
+    console.log("The number" + number4 + "is even");
+} else {
+    console.log("The number" + number4 + "is odd");
+}
