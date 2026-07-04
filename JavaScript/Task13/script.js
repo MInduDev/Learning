86681bb00c4f7403b193aa1133b498acdef2222a
M Indu <maddhi.indu@gmail.com>
2026-07-04 11:27:29 +0530

    Update script.js

--- JavaScript/Task13/script.js
+++ JavaScript/Task13/script.js
@@ -1,28 +1,28 @@
 let number1 = 5;
-let number1 = 10;
-let number1 = 15;
-let number1 = 20;
+let number2 = 10;
+let number3 = 15;
+let number4 = 20;
 
-if (number1 % 2 === 0){
-    console.log("The number" + number1 + "is even");
+if (number1 % 2 === 0) {
+    console.log("The number " + number1 + " is even");
 } else {
-    console.log("The number" + number1 + "is odd");
+    console.log("The number " + number1 + " is odd");
 }
 
-if (number2 % 2 === 0){
-    console.log("The number" + number2 + "is even");
+if (number2 % 2 === 0) {
+    console.log("The number " + number2 + " is even");
 } else {
-    console.log("The number" + number2 + "is odd");
+    console.log("The number " + number2 + " is odd");
 }
 
-if (number3 % 2 === 0){
-    console.log("The number" + number3 + "is even");
+if (number3 % 2 === 0) {
+    console.log("The number " + number3 + " is even");
 } else {
-    console.log("The number" + number3 + "is odd");
+    console.log("The number " + number3 + " is odd");
 }
 
-if (number4 % 2 === 0){
-    console.log("The number" + number4 + "is even");
+if (number4 % 2 === 0) {
+    console.log("The number " + number4 + " is even");
 } else {
-    console.log("The number" + number4 + "is odd");
+    console.log("The number " + number4 + " is odd");
 }
