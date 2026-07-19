1b7e67cc46e1e5c9f032c631953b514d38afe432
M Indu <maddhi.indu@gmail.com>
2026-07-19 14:12:26 +0530

    Update script.js

--- JavaScript/Task19/script.js
+++ JavaScript/Task19/script.js
@@ -1,257 +1,400 @@
-// Services Data
+// Laundry Services
+const services = [
+    {
+        id: 1,
+        name: "Dry Cleaning",
+        price: 200,
+        image: "images/dry-cleaning.png"
+    },
 
-const services=[
     {
-        id:1,
-        name:"Dry Cleaning",
-        price:200,
-        image:"images/dry-cleaning.png"
+        id: 2,
+        name: "Leather & Suede Cleaning",
+        price: 400,
+        image: "images/leather-cleaning.png"
     },
+
     {
-        id:2,
-        name:"Leather & Suede Cleaning",
-        price:150,
-        image:"images/leather-cleaning.png"
+        id: 3,
+        name: "Ironing",
+        price: 50,
+        image: "images/ironing.png"
     },
+
     {
-        id:3,
-        name:"Ironing",
-        price:50,
-        image:"images/ironing.png"
+        id: 4,
+        name: "Wedding Dress Cleaning",
+        price: 700,
+        image: "images/wedding-cleaning.png"
     },
+
     {
-        id:4,
-        name:"Wedding Dress Cleaning",
-        price:500,
-        image:"images/wedding-cleaning.png"
+        id: 5,
+        name: "Wash and Fold",
+        price: 70,
+        image: "images/wash-fold.png"
     },
+
     {
-        id:5,
-        name:"Wash and Fold",
-        price:300,
-        image:"images/wash-fold.png"
-    },
-    {
-        id:6,
-        name:"Stain Removal",
-        price:150,
-        image:"images/stain-removal.png"
+        id: 6,
+        name: "Stain Removal",
+        price: 150,
+        image: "images/stain-removal.png"
     }
 ];
 
-let cart=[];
+// Array
+let cart = [];
+let skippedServices = [];
 
-const serviceContainer = document.getElementById("serviceList");
-const cartTable = document.getElementById("cartItems");
-const totalPrice = document.getElementById("total");
+// HTML Elements
+const serviceList = document.getElementById("serviceList");
+const cartItems = document.getElementById("cartItems");
 const emptyCart = document.getElementById("emptyCart");
-
+const totalPrice = document.getElementById("totalPrice");
 const bookingForm = document.getElementById("bookingForm");
 const message = document.getElementById("message");
 const cartBtn = document.getElementById("cartBtn");
 const bookNowBtn = document.getElementById("bookNowBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const cartSection = document.querySelector(".cart");
+const bookingSection = document.querySelector(".booking");
 
+// Form Input
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("Password");
+
+// Display All Services
 function showServices(){
+    serviceList.innerHTML = "";
+    services.forEach(function(service){
+        if(skippedServices.includes(service.id)){
+            return;
+        }
 
-serviceContainer.innerHTML="";
+        // Card
+        const card = document.createElement("div");
+        card.classList.add("service");
 
-for(let i=0;i<services.length;i++){
+        // Image
+        const image = document.createElement("img");
+        image.src = service.image;
+        image.alt = service.name;
 
-let card=document.createElement("div");
-card.className="service-card";
+        // Name
+        const title = document.createElement("h3");
+        title.textContent = service.name;
 
-card.innerHTML=`
-<img src="${services[i].image}" alt="${services[i].name}">
+        // Price
+        const price = document.createElement("p");
+        price.textContent = "₹" + service.price;
 
-<h3>${services[i].name}</h3>
+        // Button Container
+        const buttonBox = document.createElement("div");
+        buttonBox.classList.add("buttons");
 
-<p>₹${services[i].price}</p>
+        // Skip Button
+        const skipButton = document.createElement("button");
+        skipButton.textContent = "Skip Item";
+        skipButton.classList.add("skip");
 
-<div class="card-btns">
-<button class="skip-btn" onclick="skipItem(${services[i].id})">
-Skip Item
-</button>
+        // Add Button
+        const addButton = document.createElement("button");
+        addButton.textContent = "Add Item";
+        addButton.classList.add("add");
 
-<button class="add-btn" onclick="addItem(${services[i].id})">
-Add Item
-</button>
-</div>
-`;
+        // Disable Add Button if already added
+        const added = cart.find(function(item){
+            return item.id === service.id;
+        });
 
-serviceContainer.appendChild(card);
+        if(added){
+            addButton.disabled = true;
+            addButton.textContent = "Added";
+        }
 
+        // skip Event
+        skipButton.addEventListener("click", function(){
+            skipService(service.id);
+        });
+
+        // Add Event 
+        addButton.addEventListener("click", function(){
+            addService(service.id);
+        });
+
+        // Append Button
+        buttonBox.appendChild(skipButton);
+        buttonBox.appendChild(addButton);
+
+        // Append Card
+        card.appendChild(image);
+        card.appendChild(title);
+        card.appendChild(price);
+        card.appendChild(buttonBox);
+
+        // Append to Service List
+        serviceList.appendChild(card);
+    });
 }
 
-}
 
-function skipItem(id){
+// Add Service To Cart
+function addService(id){
 
-let card=document.querySelectorAll(".service-card");
-
-for(let i=0;i<services.length;i++){
-
-if(services[i].id===id){
-card[i].style.display="none";
-break;
-}
-
-}
-
-}
-
-// Add Item to Cart
-
-function addItem(id){
-
-     let service = services.find(item => item.id === id);
-     let alreadyAdded = cart.find(item => item.id === id);
-     if(alreadyAdded){
-        alert("Services already added.");
+    // Check if service is skipped
+    if(skippedServices.includes(id)){
+        alert("This service has already been skipped.");
         return;
-     }
+    }
 
-     if (service) {
-        cart.push(service);
+    // Check if Item Already Added
+    const alreadyAdded = cart.find(function(item){
+        return item.id === id;
+    });
 
-     }
-     
-     saveCart();
-     updateCart();
+    if(alreadyAdded){
+        alert("Service is already in the cart.");
+        return;
+    }
 
+    // Find Selected Service
+    const selectedService = services.find(function(service){
+        return service.id === id;
+    });
+
+    if(!selectedService){
+        return;
+    }
+
+    // Add to Cart
+    cart.push(selectedService);
+
+    // save Cart
+    saveCart();
+
+    // Refresh Cart
+    updateCart();
+
+    // Show Service
+    showServices();
+}
+
+// Save Cart
+function saveCart(){
+    localStorage.setItem("cartItems", JSON.stringify(cart));
+    localStorage.setItem("skippedServices", JSON.stringify(skippedServices));
+}
+
+// Load Cart
+function loadCart(){
+    const savedCart = localStorage.getItem("cartItems");
+    const savedSkipped = localStorage.getItem("skippedServices");
+
+    if(savedCart){
+        cart = JSON.parse(savedCart);
+    }
+
+    if(savedSkipped){
+        skippedServices = JSON.parse(savedSkipped);
+    }
+
+    updateCart();
+    showServices();
 }
 
 
-function removeItem(index){
+// Skip Service
+function skipService(id){
+    
+    // Confirmation
+    if(!confirm("Skip this service permanently?")){
+        return;
+    }
+    
+    //Check if already Skipped
+    if(skippedServices.includes(id)){
+        return;
+    } 
 
-cart.splice(index,1);
+    // Save Skipped Service Id
+    skippedServices.push(id);
 
-saveCart();
-updateCart();
+    // Remove from cart if already added
+    cart = cart.filter(function(item){
+        return item.id !== id;
+    });
 
+    // Save update cart
+    saveCart();
+
+    // Refresh page data
+    showServices();
+    updateCart();
 }
 
+
+// Update Cart
 function updateCart(){
 
-cartTable.innerHTML="";
+    // Clear old rows
+    cartItems.innerHTML = "";
+    let total = 0;
 
-let totalAmount=0;
+    // Empty cart
+    if(cart.length === 0){
+        emptyCart.style.display = "block";
+    }
+    else{
+        emptyCart.style.display = "none";
+    }
 
-if(cart.length==0){
+    // Create Rows
+    cart.forEach(function(service,index){
+        total += service.price;
+        const row = document.createElement("tr");
 
-emptyCart.style.display="block";
+        // Serial Number
+        const number = document.createElement("td");
+        number.textContent = index + 1;
 
-}else{
+        // Service Name 
+        const serviceName = document.createElement("td");
+        serviceName.textContent = service.name;
 
-emptyCart.style.display="none";
+        // Price
+        const price = document.createElement("td");
+        price.textContent = "₹" + service.price;
 
+        // Remove Button Cell
+        const removCell = document.createElement("td");
+        const removeBtn = document.createElement("button");
+        removeBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+        // Remove Event
+        removeBtn.addEventListener("click", function(){
+            removeService(index);
+        });
+
+        removCell.appendChild(removeBtn);
+
+        // Append data
+        row.appendChild(number);
+        row.appendChild(serviceName);
+        row.appendChild(price);
+        row.appendChild(removCell);
+
+        cartItems.appendChild(row);
+    });
+
+    // Update Total
+    totalPrice.textContent = "₹" + total;
 }
 
-for(let i=0;i<cart.length;i++){
+// Remove Service
+function removeService(index){
 
-let row=document.createElement("tr");
-
-totalAmount=totalAmount+cart[i].price;
-
-row.innerHTML=`
-<td>${i+1}</td>
-<td>${cart[i].name}</td>
-<td>₹${cart[i].price}</td>
-<td>
-<button onclick="removeItem(${i})">
-<i class="fa-solid fa-trash"></i>
-</button>
-</td>
-`;
-
-cartTable.appendChild(row);
-
+    if(!confirm("Remove this service?")){
+        return;
+    }
+    cart.splice(index,1);
+    saveCart();
+    updateCart();
+    showServices();
 }
 
-totalPrice.innerHTML="₹"+totalAmount;
-
-}
-
-
-// Load saved cart when when website opens.
-
-function saveCart(){
-
-localStorage.setItem("cartItems",JSON.stringify(cart));
-
-}
-
-function loadCart(){
-
-let data=localStorage.getItem("cartItems");
-
-if(data!=null){
-
-cart=JSON.parse(data);
-
-}
-
-updateCart();
-
-}
-
+// View Cart Button
 cartBtn.addEventListener("click",function(){
-
-if(cart.length==0){
-
-alert("Please add at least one service first.");
-return;
-
-}
-
-alert("Your selected services are already added to the cart.");
-
+    cartSection.scrollIntoView({
+        behavior:"smooth",
+        block:"start"
+    });
 });
 
+// Go to booking button
 bookNowBtn.addEventListener("click",function(){
-
-document.getElementById("name").focus();
-
+    bookingSection.scrollIntoView({
+        behavior:"smooth",
+        block:"start"
+    });
 });
 
+// Booking form submit
+bookingForm.addEventListener("submit",function(event){
+    event.preventDefault();
 
-// Booking Form Event Listener
+    // Validate cart
+    if(cart.length === 0){
+        message.style.color = "red";
+        message.textContent = "Please add at least one service before booking.";
+        return;
+    }
 
-bookingForm.addEventListener("submit",function(e){
+    // Validate inputs
+    if(fullName.value.trim() === ""){
+        message.style.color = "red";
+        message.textContent = "Please enter your full name.";
+        fullName.focus();
+        return;
+    }
 
-e.preventDefault();
+    // Email empty
+    if(email.value.trim() === ""){
+        message.style.color = "red";
+        message.textContent = "Please enter your email.";
+        email.focus();
+        return;
+    }
 
-let name=document.getElementById("name").value.trim();
-let email=document.getElementById("email").value.trim();
-let password=document.getElementById("password").value.trim();
+    // Email format validation
+    if(!email.value.includes("@") || !email.value.includes(".")){
+        message.style.color = "red";
+        message.textContent = "Please enter a valid email.";
+        email.focus();
+        return;
+    }
 
-if(cart.length==0){
-message.style.color="red";
-message.innerHTML="Please add at least one service.";
-return;
-}
+    // Password empty 
+    if(password.value.trim() === ""){
+        message.style.color = "red";
+        message.textContent = "Please enter your password.";
+        password.focus();
+        return;
+    }
 
-if(name==""||email==""||password==""){
-message.style.color="red";
-message.innerHTML="Please fill all fields.";
-return;
-}
+    // Password length validation
+    if(password.value.length < 6){
+        message.style.color = "red";
+        message.textContent = "Password should contain at least 6 characters.";
+        password.focus();
+        return;
+    }
 
-message.style.color="green";
-message.innerHTML="Booking completed successfully.";
+    // Success message
+    message.style.color = "green";
+    message.textContent = "Thank you! Your service booking has been submitted successfully.";
 
-bookingForm.reset();
+    // Clear cart
+    cart = [];
 
-cart=[];
-saveCart();
-updateCart();
+    // Save update cart
+    saveCart();
 
+    // Update UI
+    updateCart();
+    showServices();
+
+    // Clear form
+    bookingForm.reset();
 });
 
-document.getElementById("logoutBtn").addEventListener("click",function(){
-
-alert("Logout Successful");
-
+// Logout button
+logoutBtn.addEventListener("click",function(){
+    localStorage.removeItem("cartItems");
+    localStorage.removeItem("skippedServices");
+    location.reload();
 });
 
-showServices();
+// Load saved data on page load
 loadCart();
